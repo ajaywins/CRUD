7b15f13375b72b479916a74fdbaa20152fbaf1f1
Akash Grover <akash.grover@ditstek.com>
2022-05-10 17:40:45 +0530

    CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import App from './app';
 import dotenv from 'dotenv'
-// import {PORT} from "./env";
+
 dotenv.config()
 let Port:any= process.env.PORT||4000
-// let Port:any = PORT
+
 new App(Port).listen();
 
 
